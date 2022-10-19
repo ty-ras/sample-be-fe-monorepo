ada2bd5f99044199cf3cfdb1ba2bb31fe1621b52
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2022-10-19 22:52:50 +0300

    #2 Things compile and work now - endpoint specifi…
    …cation is now more compact than before.

--- backend/src/api/auxiliary/types.d.ts
+++ backend/src/api/auxiliary/types.d.ts
@@ -3,16 +3,15 @@ import type * as openapi from "@ty-ras/metadata-openapi";
 import type * as dataBE from "@ty-ras/data-backend-io-ts";
 import type * as server from "@ty-ras/server-node";
 import type * as state from "./state";
-import type * as services from "../../services";
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export type EndpointSpec<
   TProtocolSpec extends protocol.ProtocolSpecCore<string, unknown>,
-  TFunctionality extends services.Service<any, any>,
+  TFunctionality extends (...args: Array<any>) => any,
   TStateSpec extends object = { db: true },
 > = dataBE.EndpointSpec<
   TProtocolSpec,
-  TFunctionality["functionality"],
+  TFunctionality,
   server.ServerContext,
   ReadonlyArray<keyof TStateSpec & keyof state.State>,
   state.GetState<TStateSpec>,
--- backend/src/api/endpoints/things.ts
+++ backend/src/api/endpoints/things.ts
@@ -1,8 +1,6 @@
 import * as protocol from "../../protocol";
 import * as aux from "../auxiliary";
 import * as services from "../../services/things";
-import * as data from "@ty-ras/data-backend-io-ts";
-import * as t from "io-ts";
 
 export const createThingsEndpoints = (builder: aux.Builder) => [
   builder.atURL``.batchSpec(getThings).createEndpoint({
@@ -17,7 +15,7 @@ export const createThingsEndpoints = (builder: aux.Builder) => [
   }),
 ];
 
-const getThings2 = aux.createServiceEndpoint(services.getThings, {
+const getThings = aux.createServiceEndpoint(services.getThings, {
   db: true,
   username: true,
 })<protocol.APIGetThings>(
@@ -54,76 +52,35 @@ const getThings2 = aux.createServiceEndpoint(services.getThings, {
   },
 );
 
-const getThings: aux.EndpointSpec<
-  protocol.APIGetThings,
-  typeof services.getThings,
-  { db: true; username: true }
-> = {
-  state: aux.endpointState({ db: true, username: true }),
-  endpointHandler: async ({
-    state: {
-      db: { db },
-      username,
-    },
-  }) => await services.getThings.functionality(username, db),
-  method: "GET",
-  // Remember that io-ts types 'encode' is identity in all cases when data is raw JSON objects (as opposed to e.g. classes)
-  // This means that we will not do "double work" by first service function doing validation on SQL rows, and then REST API validating service function array.
-  output: data.responseBody(services.getThings.validation),
-  mdArgs: {
-    openapi: {
-      urlParameters: undefined,
-      queryParameters: undefined,
-      requestHeaders: undefined,
-      body: undefined,
-      output: {
-        description: "",
-        mediaTypes: {
-          "application/json": {
-            example: [
-              {
-                id: "DummyID",
-              },
-            ],
-          },
-        },
-      },
-      responseHeaders: undefined,
-      operation: {},
-    },
-  },
-};
-
 // Notice: this is not behind authentication.
 // Just to demonstrate how non-authenticated endpoints can still access e.g. DB
-const getThingsCount: aux.EndpointSpec<
-  protocol.APIThingsSummary,
-  typeof services.getThingsCount
-> = {
-  state: aux.endpointState({ db: true }),
-  endpointHandler: ({
+const getThingsCount = aux.createServiceEndpoint(services.getThingsCount, {
+  db: true,
+})<protocol.APIThingsSummary>(
+  ({
     state: {
       db: { db },
     },
-  }) => services.getThingsCount.functionality(db),
-  method: "GET",
-  output: data.responseBody(t.number),
-  mdArgs: {
-    openapi: {
-      urlParameters: undefined,
-      queryParameters: undefined,
-      requestHeaders: undefined,
-      body: undefined,
-      output: {
-        description: "",
-        mediaTypes: {
-          "application/json": {
-            example: 123,
+  }) => [db],
+  {
+    method: "GET",
+    mdArgs: {
+      openapi: {
+        urlParameters: undefined,
+        queryParameters: undefined,
+        requestHeaders: undefined,
+        body: undefined,
+        output: {
+          description: "",
+          mediaTypes: {
+            "application/json": {
+              example: 123,
+            },
           },
         },
+        responseHeaders: undefined,
+        operation: {},
       },
-      responseHeaders: undefined,
-      operation: {},
     },
   },
-};
+);
