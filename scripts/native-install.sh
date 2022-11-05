720e56e5a9cb84155daa2bc7cb8fcabe64435911
Stanislav Muhametsin <346799+stazz@users.noreply.github.com>
2022-11-05 21:47:59 +0200

    #13 Making the sample runnable from just-cloned r…
    …epo.

---
+++ scripts/native-install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+yarn install "$@"
+
+if [ "$(echo "${TYRAS_LIB_DIR}" | cut -c1-8)" != 'frontend' ]; then
+  # Frontend is special and has its own ESLint and TS configs.
+  cp .eslintrc.library.cjs "${TYRAS_LIB_DIR}/.eslintrc.cjs"
+  cp tsconfig.library.json "${TYRAS_LIB_DIR}/tsconfig.json"
+  cp tsconfig.build.json "${TYRAS_LIB_DIR}"
+fi
